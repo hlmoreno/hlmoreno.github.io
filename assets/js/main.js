873ed1ea8055625ffca7bf207bba2b9ce658ce4d
Henry Moreno <hlmoreno@gmail.com>
2020-06-17 15:56:43 -0500

    Minify main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,15 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+  $(".level-bar-inner").each(function () {
+    var itemWidthText = $(this).data("level");
 
-    $('.level-bar-inner').each(function () {
-        var itemWidthText = $(this).data("level");
-
-        if (itemWidthText == "Beginner") {
-            $(this).css("width", "33%");
-        }
-        else if (itemWidthText == "Intermediate") {
-            $(this).css("width", "66%");
-        }
-        else if (itemWidthText == "Advanced") {
-            $(this).css("width", "100%");
-        }
-        else {
-            $(this).css("width", itemWidthText);
-        }
-    });
+    if (itemWidthText == "Beginner") {
+      $(this).css("width", "33%");
+    } else if (itemWidthText == "Intermediate") {
+      $(this).css("width", "66%");
+    } else if (itemWidthText == "Advanced") {
+      $(this).css("width", "100%");
+    } else {
+      $(this).css("width", itemWidthText);
+    }
+  });
 });
